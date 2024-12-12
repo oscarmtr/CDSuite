32ba7ee15f9d83222d914d0a0a55f95c529e59e8
Óscar <120526602+oscarmtr@users.noreply.github.com>
2024-12-12 14:20:04 +0100

    Update era5.download.data.CDS.py

--- era5.download.data.CDS.py
+++ era5.download.data.CDS.py
@@ -39,10 +39,11 @@ def compress_to_single_archive(output_file, files):
     print(f"Archivos comprimidos en: {output_file}")
 
 # Parámetros comunes
-months = ["01", "02", "03", 
-          "04", "05", "06", 
-          "07", "08", "09", 
-          "10", "11", "12"
+months = [
+    "01", "02", "03", 
+    "04", "05", "06", 
+    "07", "08", "09", 
+    "10", "11", "12"
 ]
 
 days = [
