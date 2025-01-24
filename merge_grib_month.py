f7967a450e03211f27429794a8b9d7eb512ef9c2
Óscar <120526602+oscarmtr@users.noreply.github.com>
2025-01-24 17:01:32 +0100

    Update and rename merge_grib.py to merge_grib_month.py
    Aclaraciones

--- merge_grib_month.py
+++ merge_grib_month.py
@@ -5,13 +5,15 @@ import shutil
 import subprocess
 from tempfile import mkdtemp
 
-# Configuración
-year_i = 2008  # Año inicio
-year_f = 2011  # Año fin
-month_i = 1  # Mes inicio (en formato numérico)
-month_f = 2  # Mes fin (en formato numérico)
+###### Inicio configuracion ######
+year_i = 1980  # Año inicio (Incluido)
+year_f = 2023  # Año fin (Incluido)
+month_i = 1  # Mes inicio
+month_f = 12  # Mes fin
+filename = "era5.tp.sfc.hour" # Nombra como se llama el archivo descargado/carpetas antes de la seccion del rango temporal
 
 directorio_principal = "/home/user/download" # Directorio que contiene a las distintas carpetas que contienen los respectivos "data.grib"
+###### Fin configuracion ######
 
 def main():
     # Crear un directorio temporal
@@ -22,9 +24,9 @@ def main():
         for year in range(year_i, year_f + 1):
             for month in range(month_i, month_f + 1):
                 month_str = f"{month:02d}"  # Asegurar formato de dos dígitos
-                carpeta = os.path.join(directorio_principal, f"era5.z.allhPa.hour.{year}.{month_str}")  
+                carpeta = os.path.join(directorio_principal, f"{filename}.{year}.{month_str}")  
                 archivo_grib = os.path.join(carpeta, "data.grib")   # Si los archivos .grib del interior de las distintas carpetas tienen un nombre distinto a "data.grib", cambialo por el nombre que tengan
-                archivo_nc = os.path.join(directorio_temporal, f"era5.z.allhPa.hour.{year}.{month_str}.nc")
+                archivo_nc = os.path.join(directorio_temporal, f"{filename}.{year}.{month_str}.nc")
 
                 # Verificar si el archivo .grib existe y procesarlo
                 if os.path.isfile(archivo_grib): 
@@ -36,7 +38,7 @@ def main():
         # Verificar si hay archivos .nc temporales antes de combinar
         archivos_nc = [os.path.join(directorio_temporal, f) for f in os.listdir(directorio_temporal) if f.endswith(".nc")]
         if archivos_nc:
-            archivo_salida = f"era5.z.allhPa.hour.{year_i}-{year_f}.nc" # Puedes cambiar el nombre del archivo final que te devolverá en el directorio ejecutado todo los "data.grib" transformados a un sólo archivo .nc final.
+            archivo_salida = f"{filename}.{year_i}-{year_f}.nc" # Puedes cambiar el nombre del archivo final que te devolverá en el directorio ejecutado todo los "data.grib" transformados a un sólo archivo .nc final.
             subprocess.run(["cdo", "mergetime", *archivos_nc, archivo_salida], check=True)
             print(f"Archivos combinados en: {archivo_salida}")
         else:
